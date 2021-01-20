79779934fa64fea14f13352412b5e09f527ef2d9
charming-c <822170415@qq.com>
2021-01-20 09:16:43 +0800

    1.20

--- app/src/main/java/com/example/cloudmusic/API.java
+++ app/src/main/java/com/example/cloudmusic/API.java
@@ -1,10 +1,12 @@
 package com.example.cloudmusic;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface API {
     @GET("users/{user}/repos")
-    Call<Repo> getRepo(@Path("user")String user);
+    Call<List<Repo>> getRepo(@Path("user")String user);
 }
--- app/src/main/java/com/example/cloudmusic/MainActivity.java
+++ app/src/main/java/com/example/cloudmusic/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.cloudmusic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -12,31 +15,36 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    public Repo repo;
+    public List<Repo> repo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.github.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        API api = retrofit.create(API.class);
-
-        Call<Repo> call = api.getRepo("liuyuxin-cloud");
-        call.enqueue(new Callback<Repo>() {
+        new Thread(){
             @Override
-            public void onResponse(Call<Repo> call, Response<Repo> response) {
-                repo = response.body();
-                Toast.makeText(MainActivity.this,"succeed",Toast.LENGTH_LONG).show();
-            }
+            public void run() {
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl("https://api.github.com/")
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+                API api = retrofit.create(API.class);
+                Call<List<Repo>> call = api.getRepo("liuyuxin-cloud");
+                call.enqueue(new Callback<List<Repo>>() {
+                    @Override
+                    public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
+                        Log.d("tag","lalaal");
+                        repo = response.body();
+                        Toast.makeText(MainActivity.this,"succeed",Toast.LENGTH_LONG).show();
+                    }
 
-            @Override
-            public void onFailure(Call<Repo> call, Throwable t) {
-                t.printStackTrace();
-                Toast.makeText(MainActivity.this,"error",Toast.LENGTH_LONG).show();
+                    @Override
+                    public void onFailure(Call<List<Repo>> call, Throwable t) {
+                        t.printStackTrace();
+                        Toast.makeText(MainActivity.this,"error",Toast.LENGTH_LONG).show();
+                    }
+                });
             }
-        });
+        }.start();
     }
 }
