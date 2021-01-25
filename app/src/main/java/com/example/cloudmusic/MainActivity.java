f20b9d62ede6d36ce8a242f6e12dd3037d689193
charming-c <822170415@qq.com>
2021-01-25 17:03:26 +0800

    BottomNavigationView和主页的一些布局

--- app/src/main/java/com/example/cloudmusic/MainActivity.java
+++ app/src/main/java/com/example/cloudmusic/MainActivity.java
@@ -21,30 +21,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Thread(){
-            @Override
-            public void run() {
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl("https://api.github.com/")
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build();
-                API api = retrofit.create(API.class);
-                Call<List<Repo>> call = api.getRepo("liuyuxin-cloud");
-                call.enqueue(new Callback<List<Repo>>() {
-                    @Override
-                    public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
-                        Log.d("tag","lalaal");
-                        repo = response.body();
-                        Toast.makeText(MainActivity.this,"succeed",Toast.LENGTH_LONG).show();
-                    }
 
-                    @Override
-                    public void onFailure(Call<List<Repo>> call, Throwable t) {
-                        t.printStackTrace();
-                        Toast.makeText(MainActivity.this,"error",Toast.LENGTH_LONG).show();
-                    }
-                });
-            }
-        }.start();
     }
 }
